3987c3a8cd5d6d86e72879c8ec18f1f90eeb4ca2
Firefly <130263671+3100587120@users.noreply.github.com>
2026-08-09 16:21:40 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,14 @@
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    // 如果这里没有，就在下面 buildscript 里加
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.7.0")   // 原来是 8.2.0，改为 8.7.0
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")  // 保持不变
+    }
 }

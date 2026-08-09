a914158fd565f1879000758544a9d03d4d735c93
Firefly <130263671+3100587120@users.noreply.github.com>
2026-08-09 17:35:35 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    
+    // 👇 新增这一行，解决 Unresolved reference: viewModels 报错
+    implementation("androidx.fragment:fragment-ktx:1.7.0") 
 }
